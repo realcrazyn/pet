30e9b359161a0c0abfcb0d1968860c442ab8a0ea
Your Name <you@example.com>
2023-02-19 20:17:01 +0300

    fix

--- client/src/http/http.ts
+++ client/src/http/http.ts
@@ -1,4 +1,7 @@
 //=============================== SHOP =====================================
 
-export const httpShopProducts = 'shop/products'
-export const httpShopProduct = 'shop/product'
+// export const httpShopProducts = 'shop/products'
+// export const httpShopProduct = 'shop/product'
+
+export const httpShopProducts = 'api/v1/products'
+export const httpShopProduct = 'api/v1/products'
