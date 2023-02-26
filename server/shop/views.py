8c7522eaa1b40c42aa2b3eed56edb5424160cef8
droxbrl <rustanovich01@gmail.com>
2023-02-26 23:53:54 +0400

    post request for /api/v1/products/
    with filter 'categories'

--- server/shop/views.py
+++ server/shop/views.py
@@ -12,11 +12,34 @@ class ProductList(APIView):
         return Response(serializer.data)
 
     def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # {"serach_value":"","categories":[]}
+        search_value = request.POST.get('serach_value')
+        categories = request.POST.get('categories')
+        if not search_value and not categories:
+            products_list = Product.objects.all()
+            serializer = ProductSerializer(products_list, many=True)
+            return Response(serializer.data)
+
+        if categories:
+            # todo: если исходная строка вида 'cat1 ,cat2', то не найдет по имени 'cat1 ' из-за пробела.
+            #  переделать поиск по id категории.
+            categories = categories.split(',')
+            products_list = Product.objects.filter(category__name__in=categories)
+            serializer = ProductSerializer(products_list, many=True)
+            return Response(serializer.data)
+
+        if search_value:
+            # todo: реализовать поиск по имени? продукта
+            products_list = Product.objects.all()
+            serializer = ProductSerializer(products_list, many=True)
+            return Response(serializer.data)
+
+        return Response(request.data, status=status.HTTP_201_CREATED)
+
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CategoryList(APIView):
