80059915ea844d3ebf388233e32644d7fa2e6309
droxbrl <rustanovich01@gmail.com>
2023-03-12 14:46:44 +0400

    cors fixed

--- server/server/settings.py
+++ server/server/settings.py
@@ -24,7 +24,8 @@ SECRET_KEY = 'django-insecure-29cq(22bn_px%8kynktl03ehg=p1)mrz2)-au75o+=)@ln7&6x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['194.87.80.9', 'biba-i-boba-pet.ru', '127.0.0.1']
+# ALLOWED_HOSTS = ['194.87.80.9', 'biba-i-boba-pet.ru', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -43,15 +44,16 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
@@ -130,6 +132,8 @@ REST_FRAMEWORK = {
     ),
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://biba-i-boba-pet.ru',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://biba-i-boba-pet.ru',
+# ]
+
+CORS_ORIGIN_ALLOW_ALL = True
