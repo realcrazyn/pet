ee482faa528a990b03fdab0d3f0348c1f482431f
Your Name <you@example.com>
2023-03-13 19:43:28 +0300

    fix

--- client/src/http/http.ts
+++ client/src/http/http.ts
@@ -9,6 +9,6 @@ export const httpAuthRegister = 'api/v1/user/register'
 // export const httpShopProduct = 'shop/product'
 // export const httpShopCategories = 'shop/categories'
 
-export const httpShopCategories = 'api/v1/categories'
+export const httpShopCategories = 'api/v1/categories/'
 export const httpShopProducts = 'api/v1/products/'
 export const httpShopProduct = 'api/v1/product/'
